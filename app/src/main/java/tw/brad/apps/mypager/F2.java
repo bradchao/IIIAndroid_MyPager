36c05d5b316fae24b1ec035c60e76182f9bf41f0
bradchao <bk400288>
2020-07-25 15:28:07 +0800

    072507

--- app/src/main/java/tw/brad/apps/mypager/F2.java
+++ app/src/main/java/tw/brad/apps/mypager/F2.java
@@ -18,7 +18,25 @@ public class F2 extends Fragment {
         if (mainView == null){
             mainView = inflater.inflate(R.layout.fragment_f2, container, false);
             flipper = mainView.findViewById(R.id.flipper);
+            View f0 = flipper.getChildAt(0);
+            View f1 = flipper.getChildAt(1);
+            View f2 = flipper.getChildAt(2);
+            View f3 = flipper.getChildAt(3);
+            FlipperListener listener = new FlipperListener();
+            f0.setOnClickListener(listener);
+            f1.setOnClickListener(listener);
+            f2.setOnClickListener(listener);
+            f3.setOnClickListener(listener);
+
         }
         return mainView;
     }
+
+    private class FlipperListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            flipper.showNext();
+        }
+    }
+
 }
