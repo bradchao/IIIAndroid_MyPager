85d6e53af9c0c0bc515b781bce4f06c88af62d2b
bradchao <bk400288>
2020-07-25 14:29:15 +0800

    072504

--- app/src/main/java/tw/brad/apps/mypager/MainActivity.java
+++ app/src/main/java/tw/brad/apps/mypager/MainActivity.java
@@ -1,44 +1,58 @@
 package tw.brad.apps.mypager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    private FragmentManager manager;
     private F1 f1;
     private F2 f2;
     private F3 f3;
+    private ViewPager viewPager;
+    private Fragment[] fs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        manager = getSupportFragmentManager();
-        f1 = new F1(); f2 = new F2(); f3 = new F3();
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.container, f1);
-        transaction.commit();
+        f1 = new F1();f2 = new F2();f3 = new F3();
+        fs = new Fragment[]{f1, f2, f3};
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class MyPagerAdapter extends FragmentStatePagerAdapter {
+        public MyPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fs[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fs.length;
+        }
     }
 
     public void gotoF1(View view) {
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.container, f1);
-        transaction.commit();
     }
     public void gotoF2(View view) {
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.container, f2);
-        transaction.commit();
     }
     public void gotoF3(View view) {
-        FragmentTransaction transaction = manager.beginTransaction();
-        transaction.replace(R.id.container, f3);
-        transaction.commit();
     }
 }
